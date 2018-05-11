08eb2c7c89a15b28ec9651aa289b5c90eb07a2d2
Sofiane MEDJKOUNE <sofiane@medjkoune.fr>
2018-05-11 23:13:18 +0200

    Modifications pour 3.5 sur le setup.py

--- setup.py
+++ setup.py
@@ -5,31 +5,34 @@ def read(f):
     with open(f) as file:
         return file.read()
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-version = str(_version_re.search(read('ash/__init__.py').decode('utf-8')).group(1)).strip("'")
+def get_version():
+    _version_re = re.compile(r'__version__\s+=\s+(.*)')
+    _to_parse = read('ash/__init__.py')
+    version = _version_re.search(_to_parse).group(1).strip("'")
+    return version
 
 setup(name='ash',
-      version=version,
-      description='The Ansible SHell',
-      # long_description=read('README.rd') + '\n\n' + read('HISTORY.rd'),
-      classifiers=[
+    version = get_version(),
+    description = 'The Ansible SHell',
+    # long_description=read('README.rd') + '\n\n' + read('HISTORY.rd'),
+    classifiers = [
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        'License :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Ansible :: Shell :: cli',
-      ],
-      keywords='ash ansible shell cli prompt-toolkit pt',
-      url='https://github.com/Nani-o/ash',
-      author='Sofiane Medjkoune',
-      author_email='sofiane@medjkoune.fr',
-      license='MIT',
-      packages=['ash'],
-      install_requires=[
-          'ansible',
-          'prompt_toolkit'
-      ],
-      entry_points={
-          'console_scripts': ['ash=ash.main:main'],
-      },
-      include_package_data=True,
-      zip_safe=False)
+    ],
+    keywords = 'ash ansible shell cli prompt-toolkit pt',
+    url = 'https://github.com/Nani-o/ash',
+    author = 'Sofiane Medjkoune',
+    author_email = 'sofiane@medjkoune.fr',
+    license = 'MIT',
+    packages = ['ash'],
+    install_requires = [
+        'ansible',
+        'prompt_toolkit'
+    ],
+    entry_points = {
+        'console_scripts': ['ash=ash.main:main'],
+    },
+    include_package_data = True,
+    zip_safe = False)
